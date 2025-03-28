ed3b06d6f92c17aec328202d9440e022f0bc2e65
Marek Ceľuch <celuchmarek@gmail.com>
2025-03-28 15:26:11 +0100

    add responsible subject role

---
+++ db/migrate/20250328145618_add_responsible_subject_role.rb
@@ -0,0 +1,20 @@
+class AddResponsibleSubjectRole < ActiveRecord::Migration[6.1]
+  def up
+    return unless Setting.exists?(name: 'system_init_done')
+
+    new_role = Role.find_or_initialize_by(name: 'Responsible Subject').tap do |role|
+      role.note = __('Responsible Subject users.')
+      role.default_at_signup = false
+      role.preferences = {}
+      role.updated_by_id = 1
+      role.created_by_id = 1
+    end
+    new_role.permission_grant('ticket.customer')
+
+    new_role.save!
+  end
+
+  def down
+    Role.find_by(name: 'Responsible Subject')&.destroy
+  end
+end
---
+++ db/seeds/ops_custom_roles.rb
@@ -0,0 +1,9 @@
+responsible_subject_role = Role.find_or_initialize_by(name: 'Responsible Subject').tap do |role|
+  role.note = __('Responsible Subject users.')
+  role.default_at_signup = false
+  role.preferences = {}
+  role.updated_by_id = 1
+  role.created_by_id = 1
+end
+responsible_subject_role.permission_grant('ticket.customer')
+responsible_subject_role.save!
