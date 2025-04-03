7b4bee230962ceb1e5c36d36555040ce600feb0c
Jan Suchal <jan.suchal@gmail.com>
2025-04-04 00:35:06 +0200

    Add banned field to user visible to admins only

--- zammad/db/seeds/ops_custom_user_fields.rb
+++ zammad/db/seeds/ops_custom_user_fields.rb
@@ -1,5 +1,30 @@
 ObjectManager::Attribute.add(
   object: 'User',
+  name: 'banned',
+  display: 'Zakázaný prístup',
+  data_type: 'boolean',
+  data_option: {
+    options: {
+      false: 'nie',
+      true: 'áno'
+    },
+    default: false,
+    translate: false,
+    null: true,
+  },
+  editable: true,
+  active: true,
+  screens: {
+    edit: { 'admin.user' => { shown: true } },
+    view: { 'admin.user' => { shown: true } },
+  },
+  position: 1900,
+  created_by_id: 1,
+  updated_by_id: 1
+)
+
+ObjectManager::Attribute.add(
+  object: 'User',
   name: 'origin',
   display: __('Zdroj používateľa'),
   data_type: 'select',
