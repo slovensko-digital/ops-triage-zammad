1c7ebdfc379f8eae450105f1ad87479da8d1f0ca
Marek Ceľuch <celuchmarek@gmail.com>
2025-04-07 16:50:31 +0200

    lint

--- zammad/db/migrate/20250407164610_ops_add_investment_to_ticket.rb
+++ zammad/db/migrate/20250407164610_ops_add_investment_to_ticket.rb
@@ -3,33 +3,33 @@ class OpsAddInvestmentToTicket < ActiveRecord::Migration[7.1]
     return unless Setting.exists?(name: 'system_init_done')
 
     ObjectManager::Attribute.add(
-    object: 'Ticket',
-    name: 'investment',
-    display: __('Investičná akcia'),
-    data_type: 'boolean',
-    data_option: {
-      options: { false => 'nie', true => 'áno' },
-      default: false,
-      null: true,
-      relation: ''
-    },
-    active: true,
-    screens: {
-      create_middle: {
-        'ticket.customer' => { shown: false },
-        'ticket.agent' => { shown: false }
+      object: 'Ticket',
+      name: 'investment',
+      display: __('Investičná akcia'),
+      data_type: 'boolean',
+      data_option: {
+        options: { false => 'nie', true => 'áno' },
+        default: false,
+        null: true,
+        relation: ''
       },
-      edit: {
-        'ticket.customer' => { shown: false },
-        'ticket.agent' => { shown: false }
-      }
-    },
-    position: 61,
-    created_by_id: 1,
-    updated_by_id: 1
-  )
+      active: true,
+      screens: {
+        create_middle: {
+          'ticket.customer' => { shown: false },
+          'ticket.agent' => { shown: false }
+        },
+        edit: {
+          'ticket.customer' => { shown: false },
+          'ticket.agent' => { shown: false }
+        }
+      },
+      position: 61,
+      created_by_id: 1,
+      updated_by_id: 1
+    )
 
-  ObjectManager::Attribute.migration_execute
+    ObjectManager::Attribute.migration_execute
   end
 
   def down
