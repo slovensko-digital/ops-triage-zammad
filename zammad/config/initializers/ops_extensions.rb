e1ac696262622296982181eb715d251d574ede09
Jan Suchal <jan.suchal@gmail.com>
2025-05-21 21:47:43 +0200

    Add support for sending parameterized email notifications from triggers

--- zammad/config/initializers/ops_extensions.rb
+++ zammad/config/initializers/ops_extensions.rb
@@ -3,4 +3,5 @@ ActiveSupport.on_load(:after_initialize) do
   Controllers::AttachmentsControllerPolicy.prepend Ops::AttachmentsControllerPolicyExtensions
   Ticket::Article::TriggersSubscriptions.prepend Ops::TicketArticleTriggersSubscriptionsExtensions
   User.prepend Ops::UserExtensions
+  Ticket::PerformChanges::Action::NotificationEmail.prepend Ops::TicketPerformChangesActionNotificationEmailExtensions
 end
