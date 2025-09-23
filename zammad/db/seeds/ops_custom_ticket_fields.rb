a234d69fd8c5b0b6f81400ed9762ba854c7d610a
Marek Ceľuch <celuchmarek@gmail.com>
2025-09-23 13:19:18 +0200

    update verification process

--- zammad/db/seeds/ops_custom_ticket_fields.rb
+++ zammad/db/seeds/ops_custom_ticket_fields.rb
@@ -25,6 +25,37 @@ ObjectManager::Attribute.add(
 
 ObjectManager::Attribute.add(
   object: 'Ticket',
+  name: 'issue_resolved',
+  display: __('Vyriešený podnet'),
+  data_type: 'select',
+  data_option: {
+    options: {
+      "yes" => "Áno",
+      "no" => "Nie"
+    },
+    default: '',
+    maxlength: 255,
+    nulloption: true,
+    null: true,
+  },
+  active: true,
+  screens: {
+    create_middle: {
+      'ticket.customer' => { shown: false },
+      'ticket.agent' => { shown: false }
+    },
+    edit: {
+      'ticket.customer' => { shown: false },
+      'ticket.agent' => { shown: false }
+    }
+  },
+  position: 16,
+  created_by_id: 1,
+  updated_by_id: 1
+)
+
+ObjectManager::Attribute.add(
+  object: 'Ticket',
   name: 'responsible_subject',
   display: __('Zodpovedný subjekt'),
   data_type: 'autocompletion_ajax_external_data_source',
